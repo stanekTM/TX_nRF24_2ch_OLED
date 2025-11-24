9231a00a417927eb4ccce1c5d67cc56dbe77a67e
stanekTM <44894972+stanekTM@users.noreply.github.com>
2025-11-24 17:55:37 +0100

    Update TX_nRF24_2ch_OLED.ino

--- TX_nRF24_2ch_OLED/TX_nRF24_2ch_OLED.ino
+++ TX_nRF24_2ch_OLED/TX_nRF24_2ch_OLED.ino
@@ -11,10 +11,12 @@
   **************************************************************************************************************
 */
 
-#include "RF24.h"    // v1.4.11
-#include "SPI.h"
-#include "EEPROM.h"
-#include "U8g2lib.h" // v2.33.15
+#include <RF24.h>    // v1.4.11
+#include <RF24_config.h>
+#include <nRF24L01.h>
+#include <SPI.h>
+#include <EEPROM.h>
+#include <U8g2lib.h> // v2.33.15
 #include "Config.h"  // Load static and variable configuration settings
 
 //**************************************************************************************************************
