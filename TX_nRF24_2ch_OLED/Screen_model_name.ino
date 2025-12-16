9155abd8317a85b439ea12b60af00437430a7d62
stanekTM <44894972+stanekTM@users.noreply.github.com>
2025-12-17 00:53:46 +0100

    Add model name screen display implementation
    Introduces draw_model_name_screen and model_name_screen functions for rendering the model name screen using a state machine approach. The screen displays the model name, current model number, and highlights the selected character.

---
+++ TX_nRF24_2ch_OLED/Screen_model_name.ino
@@ -0,0 +1,81 @@
+
+//*********************************************************************************************************************
+// Drawing MODEL NAME screen display
+//*********************************************************************************************************************
+// This is the state machine, which will replace the do - while loop
+void draw_model_name_screen()
+{
+  static uint8_t is_next_page = 0;
+  
+  // Call to first page, if required
+  if (is_next_page == 0)
+  {
+    u8g2.firstPage();
+    is_next_page = 1;
+  }
+  
+  // Draw our screen
+  model_name_screen();
+  
+  // Call to next page
+  if (u8g2.nextPage() == 0)
+  {
+    is_next_page = 0; // Ensure, that first page is called
+  }
+}
+
+//------------------------------------------------------------------------
+void model_name_screen()
+{
+  // Set memory buffer for text strings
+  char menu_buffer[7];
+  char char_buffer[9];
+  
+  // Print "MODEL NAME"
+  strcpy_P(menu_buffer, (char*)pgm_read_word(&(menu_name[5])));
+  u8g2.setCursor(0, 9);
+  u8g2.print(menu_buffer);
+  
+  // Print number of which model in use
+  u8g2.setCursor(90, 9);
+  u8g2.print(modelActual + 1);
+  
+  // Drawing horizontal line under header
+  u8g2.drawHLine(0, 10, 128);
+  
+  
+  // Print MODEL NAME 5 byte
+  u8g2.setFont(u8g2_font_VCR_OSD_tr); // Height 15 pixels (dafont)
+  
+  for (int i = 0; i < 5; i++)
+  {
+    u8g2.setCursor(13 + (i * 23), 45);
+    u8g2.print(modelName[i]);
+    
+    if (i == menuSubActual - 1)
+    {
+      if (modelNameSelection == i)
+      {
+        // Print "("
+        strcpy_P(char_buffer, (char*)pgm_read_word(&(one_char[2])));
+        u8g2.setCursor(1 + (i * 23), 45);
+        u8g2.print(char_buffer);
+        
+        // Print ")"
+        strcpy_P(char_buffer, (char*)pgm_read_word(&(one_char[3])));
+        u8g2.setCursor(25 + (i * 23), 45);
+        u8g2.print(char_buffer);
+      }
+      else
+      {
+        // Print ">"
+        strcpy_P(char_buffer, (char*)pgm_read_word(&(one_char[5])));
+        u8g2.setCursor(1 + (i * 23), 45);
+        u8g2.print(char_buffer);
+      }
+    }
+  }
+  
+  u8g2.setFont(u8g2_font_7x13_tr); // Height 9 pixels (X11)
+}
+ 
