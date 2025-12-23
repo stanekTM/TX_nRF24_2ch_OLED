b073bb4917f6635c96f02354be5ad8e198092c87
stanekTM <44894972+stanekTM@users.noreply.github.com>
2025-12-23 13:46:07 +0100

    Refactor calc_expo parameter and variable names
    Renamed 'trim_ppm_mid' to 'trim_mid' for clarity and updated all related references in the calc_expo function. Improved comments and formatting for better readability.

--- TX_nRF24_2ch_OLED/Expo.ino
+++ TX_nRF24_2ch_OLED/Expo.ino
@@ -1,12 +1,12 @@
 
 //*********************************************************************************************************************
-// EXPO Eq.
+// EXPO equation
 // Y = X * EXPO((X - EPA) / dv))
-// proper dv value : 300 ~ 700
+// Proper dv value : 300 ~ 700
 //*********************************************************************************************************************
-int calc_expo(unsigned short trim_ppm_mid, unsigned short value, unsigned short in_max, unsigned short dv)
+int calc_expo(unsigned short trim_mid, unsigned short value, unsigned short in_max, unsigned short dv)
 {
-  if (value == trim_ppm_mid)
+  if (value == trim_mid)
   {
     return value;
   }
@@ -20,18 +20,18 @@ int calc_expo(unsigned short trim_ppm_mid, unsigned short value, unsigned short 
   
   dv = map(dv, 1, 9, 300, 700);
   
-  if (value > trim_ppm_mid)
+  if (value > trim_mid)
   {
-    value = (value - trim_ppm_mid) * exp((((double)value - trim_ppm_mid) - (in_max - trim_ppm_mid)) / dv) + trim_ppm_mid;
+    value = (value - trim_mid) * exp((((double)value - trim_mid) - (in_max - trim_mid)) / dv) + trim_mid;
     return value;
   }
   else
   {
-    unsigned short trim_value = trim_ppm_mid - MID_CONTROL_VAL;
+    unsigned short trim_value = trim_mid - MID_CONTROL_VAL;
     value = (MAX_CONTROL_VAL + trim_value) - value + (MIN_CONTROL_VAL + trim_value);
     in_max = (MAX_CONTROL_VAL + trim_value) - in_max + (MIN_CONTROL_VAL + trim_value);
     
-    value = (value - trim_ppm_mid) * exp((((double)value - trim_ppm_mid) - (in_max - trim_ppm_mid)) / dv) + trim_ppm_mid;
+    value = (value - trim_mid) * exp((((double)value - trim_mid) - (in_max - trim_mid)) / dv) + trim_mid;
     return (MAX_CONTROL_VAL + trim_value) - value + (MIN_CONTROL_VAL + trim_value);
   }
 }
