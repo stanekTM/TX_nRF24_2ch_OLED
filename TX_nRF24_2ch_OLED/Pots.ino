2b2918196a85de2b1578c3ef0182ecf6bdd67a31
stanekTM <44894972+stanekTM@users.noreply.github.com>
2026-01-22 14:26:23 +0100

    Adjust EPA and subTrim handling in pot value calculation
    Refactored the calculation of min, max, and mid EPA values to consistently include subTrim offsets. Updated mapping and expo calculations to use the new mid_epa_val, ensuring more accurate and consistent pot value processing.

--- TX_nRF24_2ch_OLED/Pots.ino
+++ TX_nRF24_2ch_OLED/Pots.ino
@@ -8,31 +8,33 @@ void read_pots()
   {
     raw_pots = analogRead(i);
     
-    int min_epa_val = 1500 - (epa[i]     * 5); // epa[0], epa[1] 1000us to 1500us
-    int max_epa_val = 1500 + (epa[i + 2] * 5); // epa[2], epa[3] 2000us to 1500us
+    int min_epa_val = (1500 - (epa[i]     * 5)) + subTrim[i]; // epa[0], epa[1] 1000us to 1500us
+    int max_epa_val = (1500 + (epa[i + 2] * 5)) + subTrim[i]; // epa[2], epa[3] 2000us to 1500us
+    
+    int mid_epa_val = MID_CONTROL_VAL + subTrim[i];
     
     // Convert analog value to pots value
     if (raw_pots < mid_pots_calib[i] - DEAD_ZONE)
     {
-      pots_value[i] = map(raw_pots, min_pots_calib[i], mid_pots_calib[i] - DEAD_ZONE, min_epa_val, MID_CONTROL_VAL);
+      pots_value[i] = map(raw_pots, min_pots_calib[i], mid_pots_calib[i] - DEAD_ZONE, min_epa_val, mid_epa_val);
       
       // Use of EXPO
-      if (expo[i] > 0) pots_value[i] = calc_expo(MID_CONTROL_VAL, pots_value[i], min_epa_val, expo[i]);
+      if (expo[i] > 0) pots_value[i] = calc_expo(mid_epa_val, pots_value[i], min_epa_val, expo[i]);
     }
     else if (raw_pots > mid_pots_calib[i] + DEAD_ZONE)
     {
-      pots_value[i] = map(raw_pots, mid_pots_calib[i] + DEAD_ZONE, max_pots_calib[i], MID_CONTROL_VAL, max_epa_val);
+      pots_value[i] = map(raw_pots, mid_pots_calib[i] + DEAD_ZONE, max_pots_calib[i], mid_epa_val, max_epa_val);
       
       // Use of EXPO
-      if (expo[i] > 0) pots_value[i] = calc_expo(MID_CONTROL_VAL, pots_value[i], max_epa_val, expo[i]);
+      if (expo[i] > 0) pots_value[i] = calc_expo(mid_epa_val, pots_value[i], max_epa_val, expo[i]);
     }
     else
     {
-      pots_value[i] = MID_CONTROL_VAL + subTrim[i];
+      pots_value[i] = mid_epa_val;
     }
     
     // Use SUB TRIM and limit to 1000us to 2000us
-    pots_value[i] = constrain(pots_value[i] /*+ subTrim[i]*/, MIN_CONTROL_VAL, MAX_CONTROL_VAL);
+    pots_value[i] = constrain(pots_value[i], MIN_CONTROL_VAL, MAX_CONTROL_VAL);
     
     // Check REVERSE and applying REVERSE value if necessary
     if (bitRead(reverse, i) == 1) pots_value[i] = (MIN_CONTROL_VAL + MAX_CONTROL_VAL) - pots_value[i];
