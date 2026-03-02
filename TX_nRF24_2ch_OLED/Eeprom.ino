f87782c82cbe601435cfa47e2be80d09643fc98d
stanekTM <44894972+stanekTM@users.noreply.github.com>
2026-03-02 16:26:51 +0100

    Reorder EEPROM fields and add debug prints
    Adjust EEPROM read/write/clear routines to use a consistent field order: modelName, EPA, EXPO, subTrim, then REVERSE. Move EXPO earlier and REVERSE after subTrim in stored_data_eeprom, save_data_eeprom, and clear_data_eeprom so layout and offsets remain aligned. Add commented Serial.println statements for eeprom_pos and values to aid debugging and clarify comments about byte positions and reverse bit meanings.

--- TX_nRF24_2ch_OLED/Eeprom.ino
+++ TX_nRF24_2ch_OLED/Eeprom.ino
@@ -7,39 +7,49 @@ unsigned char stored_data_eeprom(unsigned char mod)
   // Read which model to upload data
   if (mod == 255)
   {
-    mod = EEPROM.read(ACTUAL_MODEL_EEPROM_ADDR);
+    mod = EEPROM.read(ACTUAL_MODEL_EEPROM_ADDR); // mod 0 to 9
   }
   
   // Define start position for EEPROM read (25 * [0, 1, 2, 3, 4...])
   unsigned int eeprom_pos = NUM_BYTES_PER_MODEL * mod;
   
-  // Read model name "XXXXX" 5 byte
+  // Read model name "XXXXX"
   for (int i = 0; i < 5; i++)
   {
     modelName[i] = EEPROM.read(eeprom_pos++);
+    //Serial.println(eeprom_pos); // 1, 2, 3, 4, 5 (5 byte)
+    //Serial.println(modelName[i]);
   }
   
   // Read EPA
   for (int i = 0; i < 4; i++)
   {
     epa[i] = EEPROM.read(eeprom_pos++);
-  }
-  
-  // Read REVERSE
-  reverse = EEPROM.read(eeprom_pos++);
-  
-  // Read SUB TRIM
-  for (int i = 0; i < RC_CHANNELS; i++)
-  {
-    EEPROM.get(eeprom_pos, subTrim[i]);
-    eeprom_pos += 2;
+    //Serial.println(eeprom_pos); // 6, 7, 8, 9
+    //Serial.println(epa[i]);
   }
   
   // Read EXPO
   for (int i = 0; i < RC_CHANNELS; i++)
   {
     expo[i] = EEPROM.read(eeprom_pos++);
+    //Serial.println(eeprom_pos); // 10, 11
+    //Serial.println(expo[i]);
   }
+  
+  // Read SUB TRIM
+  for (int i = 0; i < RC_CHANNELS; i++)
+  {
+    EEPROM.get(eeprom_pos, subTrim[i]);
+    eeprom_pos += 2;
+    //Serial.println(eeprom_pos); // (12)13, (14)15 (2 bytes)
+    //Serial.println(subTrim[i]);
+  }
+  
+  // Read REVERSE
+  reverse = EEPROM.read(eeprom_pos++);
+  //Serial.println(eeprom_pos); // 16
+  //Serial.println(reverse);    // bit 0 NOR and NOR, bit 1 REV and NOR, bit 2 NOR and REV, bit 3 REV and REV
   
   // Read RC channels
   for (int i = 0; i < RC_CHANNELS; i++)
@@ -63,33 +73,43 @@ void save_data_eeprom()
   // Define start position for EEPROM write/update (25 * [0, 1, 2, 3, 4...])
   unsigned int eeprom_pos = NUM_BYTES_PER_MODEL * modelActual;
   
-  // Save model name "XXXXX" 5 byte
+  // Save model name "XXXXX"
   for (int i = 0; i < 5; i++)
   {
     EEPROM.update(eeprom_pos++, modelName[i]);
+    //Serial.println(eeprom_pos); // 1, 2, 3, 4, 5 (5 byte)
+    //Serial.println(modelName[i]);
   }
   
   // Save EPA
   for (int i = 0; i < 4; i++)
   {
     EEPROM.update(eeprom_pos++, epa[i]);
+    //Serial.println(eeprom_pos); // 6, 7, 8, 9
+    //Serial.println(epa[i]);
   }
   
-  // Save REVERSE
-  EEPROM.update(eeprom_pos++, reverse);
+  // Save EXPO
+  for (int i = 0; i < RC_CHANNELS; i++)
+  {
+    EEPROM.update(eeprom_pos++, expo[i]);
+    //Serial.println(eeprom_pos); // 10, 11
+    //Serial.println(expo[i]);
+  }
   
   // Save SUB TRIM
   for (int i = 0; i < RC_CHANNELS; i++)
   {
     EEPROM.put(eeprom_pos, subTrim[i]);
     eeprom_pos += 2;
+    //Serial.println(eeprom_pos); // (12)13, (14)15 (2 bytes)
+    //Serial.println(subTrim[i]);
   } 
   
-  // Save EXPO
-  for (int i = 0; i < RC_CHANNELS; i++)
-  {
-    EEPROM.update(eeprom_pos++, expo[i]);
-  }
+  // Save REVERSE
+  EEPROM.update(eeprom_pos++, reverse);
+  //Serial.println(eeprom_pos); // 16
+  //Serial.println(reverse);    // bit 0 NOR and NOR, bit 1 REV and NOR, bit 2 NOR and REV, bit 3 REV and REV
 }
 
 //*********************************************************************************************************************
@@ -106,33 +126,43 @@ void clear_data_eeprom()
     // Define start position for EEPROM storing (25 * [0, 1, 2, 3, 4...])
     unsigned int eeprom_pos = NUM_BYTES_PER_MODEL * j;
     
-    // Save default model name "MODEL" 5 byte
+    // Save default model name "MODEL"
     for (int i = 0; i < 5; i++)
     {
       EEPROM.update(eeprom_pos++, modelName[i]);
+      //Serial.println(eeprom_pos); // 1, 2, 3, 4, 5 (5 byte)
+      //Serial.println(modelName[i]);
     }
     
-    // Save default EPA
+    // Save default EPA 100%
     for (int i = 0; i < 4; i++)
     {
       EEPROM.update(eeprom_pos++, epa[i] + EPA_MAX);
-    }
-    
-    // Save default REVERSE
-    EEPROM.update(eeprom_pos++, reverse);
-    
-    // Save default SUB TRIM
-    for (int i = 0; i < RC_CHANNELS; i++)
-    {
-      EEPROM.put(eeprom_pos, subTrim[i]);
-      eeprom_pos += 2;
+      //Serial.println(eeprom_pos); // 6, 7, 8, 9
+      //Serial.println(epa[i]);
     }
     
     // Save default EXPO
     for (int i = 0; i < RC_CHANNELS; i++)
     {
       EEPROM.update(eeprom_pos++, expo[i]);
+      //Serial.println(eeprom_pos); // 10, 11
+      //Serial.println(expo[i]);
     }
+    
+    // Save default SUB TRIM
+    for (int i = 0; i < RC_CHANNELS; i++)
+    {
+      EEPROM.put(eeprom_pos, subTrim[i]);
+      eeprom_pos += 2;
+      //Serial.println(eeprom_pos); // (12)13, (14)15 (2 bytes)
+      //Serial.println(subTrim[i]);
+    }
+    
+    // Save default REVERSE (NOR and NOR)
+    EEPROM.update(eeprom_pos++, reverse);
+    //Serial.println(eeprom_pos); // 16
+    //Serial.println(reverse);    // bit 0
   }
 }
 
